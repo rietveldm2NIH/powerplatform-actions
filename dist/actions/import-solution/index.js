f6721c323c2fff249564e88d749d5d28205aa254
Martin Rietveld <72878996+rietveldm2NIH@users.noreply.github.com>
2024-04-01 19:57:43 -0400

    Update index.js

--- dist/actions/import-solution/index.js
+++ dist/actions/import-solution/index.js
@@ -117,9 +117,9 @@ var require_authenticate = __commonJS({
       return pac("auth", "create", "--kind", "ADMIN", ...addCredentials(credentials), ...addCloudInstance(credentials));
     }
     exports2.authenticateAdmin = authenticateAdmin;
-    function authenticateEnvironment(pac, credentials, environmentId, environmentUrl, logger) {
+    function authenticateEnvironment(pac, credentials, environment, environmentUrl, logger) {
       logger.log(`authN to env. authType:${isUsernamePassword(credentials) ? "UserPass" : "SPN"} authScheme:${isUsernamePassword(credentials) ? "" : `${credentials.scheme}`}; cloudInstance: ${credentials.cloudInstance || "<not set>"}; envUrl: ${environmentUrl}`);
-      return pac("auth", "create", ...addId(environmentId), ...addCredentials(credentials), ...addCloudInstance(credentials));
+      return pac("auth", "create", ...addId(environment), ...addCredentials(credentials), ...addCloudInstance(credentials));
     }
     exports2.authenticateEnvironment = authenticateEnvironment;
     function clearAuthentication(pac) {
